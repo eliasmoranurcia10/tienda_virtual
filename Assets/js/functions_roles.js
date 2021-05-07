9b681bbe96aaed533b339fc3c61da145f19bf799
EliasMoran <yoexereliasmoranurcia@gmail.com>
2021-05-06 23:01:04 -0500

    SECCION 08: SISTEMA DE ROLES Y PERMISOS DE USUARIO - Cap 39: Mostrar Roles de Usuario en DataTables con JSON

--- Assets/js/functions_roles.js
+++ Assets/js/functions_roles.js
@@ -1,5 +1,35 @@
+
+var tableRoles;
+
+document.addEventListener('DOMContentLoaded', function(){
+
+	tableRoles = $('#tableRoles').dataTable( {
+		"aProcessing":true,
+		"aServerSide":true,
+        "language": {
+        	"url": "//cdn.datatables.net/plug-ins/1.10.20/i18n/Spanish.json"
+        },
+        "ajax":{
+            "url": " "+base_url+"/Roles/getRoles",
+            "dataSrc":""
+        },
+        "columns":[
+            {"data":"idrol"},
+            {"data":"nombrerol"},
+            {"data":"descripcion"},
+            {"data":"status"}
+        ],
+        "resonsieve":"true",
+        "bDestroy": true,
+        "iDisplayLength": 10,
+        "order":[[0,"desc"]]  
+    });
+
+});
+
 $('#tableRoles').DataTable();
 
 function openModal(){
     $('#modalFormRol').modal('show');
 }
+
--- Views/Template/footer_admin.php
+++ Views/Template/footer_admin.php
@@ -1,5 +1,9 @@
+<script>
+  const base_url = "<?= base_url(); ?>";
+</script>
+
 <!-- Essential javascripts for application to work-->
-<script src="<?= media(); ?>/js/jquery-3.3.1.min.js"></script>
+    <script src="<?= media(); ?>/js/jquery-3.3.1.min.js"></script>
     <script src="<?= media(); ?>/js/popper.min.js"></script>
     <script src="<?= media(); ?>/js/bootstrap.min.js"></script>
     <script src="<?= media(); ?>/js/main.js"></script>
