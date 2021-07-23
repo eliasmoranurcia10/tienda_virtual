3c51df88212b932c9464555c778cfbcbc719a81a
EliasMoran <yoexereliasmoranurcia@gmail.com>
2021-07-23 04:12:34 -0500

    SECCION 10: SISTEMA LOGIN CON ROLES DE USUARIO - Cap 102: Desplegar a Producción Sistema de Usuarios

--- Views/Template/footer_admin.php
+++ Views/Template/footer_admin.php
@@ -10,7 +10,6 @@
     <!-- Font-icon css-->
     <script src="<?= media(); ?>/js/fontawesome.js"></script>
     
-    <script src="<?= media(); ?>/js/functions_admin.js"></script>
 
     <!-- The javascript plugin to display page loading on top-->
     <script src="<?= media(); ?>/js/plugins/pace.min.js"></script>
@@ -29,10 +28,10 @@
     <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
     <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
 
-    <script type="text/javascript" src="<?= media(); ?>/js/functions_admin.js?v3"></script>
+    <script type="text/javascript" src="<?= media(); ?>/js/functions_admin.js"></script>
 
     <!-- Carga los funciones scripts de cada controlador -->
-    <script src="<?= media(); ?>/js/<?= $data['page_functions_js']; ?>?v3"></script>
+    <script src="<?= media(); ?>/js/<?= $data['page_functions_js']; ?>"></script>
     
   </body>
 </html>
