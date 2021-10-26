533e348722de56fad64162e282c1e03892b9cf05
EliasMoran <yoexereliasmoranurcia@gmail.com>
2021-10-26 09:45:37 -0500

    SECCION 14: CONFIGURAR TEMPLATE - Cap 155: Cofigurar Modal y Sección Productos

--- Views/Template/header_tienda.php
+++ Views/Template/header_tienda.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="<?= media()?>/tienda/images/icons/favicon.png"/>
+	<link rel="icon" type="image/png" href="<?= media()?>/images/favicon.ico"/>
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?= media()?>/tienda/vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
