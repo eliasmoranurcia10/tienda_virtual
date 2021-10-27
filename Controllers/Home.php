1af44cd2f85c091ce102a6dae989af0d5fbd7d47
EliasMoran <yoexereliasmoranurcia@gmail.com>
2021-10-27 03:10:52 -0500

    SECCION 15: DISEÑO Y DESARROLLO DE PÁGINA DE INICIO - Cap 158: Extraer productos de la Base de Datos

--- Controllers/Home.php
+++ Controllers/Home.php
@@ -1,11 +1,12 @@
 <?php
 
     require_once('Models/TCategoria.php');
+    require_once('Models/TProducto.php');
 
     class Home extends Controllers{
 
         //Usando Traits "Ver seccion 3 cap 11"
-        use TCategoria;
+        use TCategoria, TProducto;
 
         public function __construct(){
 
@@ -21,6 +22,8 @@
             $data['page_name']  = "tienda_virtual";
             $data['slider']     = $this->getCategoriasT(CAT_SLIDER);
             $data['banner']     = $this->getCategoriasT(CAT_BANNER);
+            $data['productos']  = $this->getProductosT();
+
 
             $this->views->getView($this,"home",$data);
 
