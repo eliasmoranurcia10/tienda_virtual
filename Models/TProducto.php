33848b6db23b2b81d51068bd3104006a1b5ecab6
EliasMoran <yoexereliasmoranurcia@gmail.com>
2022-01-12 08:42:56 -0500

    SECCION 17: DISEÑO Y DESARROLLO DE PÁGINA DETALLE PRODUCTO - Cap 168: Sistemas Tienda Virtual

--- Models/TProducto.php
+++ Models/TProducto.php
@@ -26,7 +26,8 @@
                         FROM producto p
                         INNER JOIN categoria c
                         ON p.categoriaid = c.idcategoria
-                        WHERE p.status != 0 ";
+                        WHERE p.status != 0
+                        ORDER BY p.idproducto DESC ";
 
             $request= $this->con->select_all($sql);
 
