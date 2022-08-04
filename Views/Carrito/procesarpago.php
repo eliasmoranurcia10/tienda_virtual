eaba9be2cf4fa8c6bf894238bee1de9b43685479
EliasMoran <yoexereliasmoranurcia@gmail.com>
2022-08-04 17:16:56 -0500

    SECCION 19: CARRITO DE COMPRAS - Cap 189: Login y Resgistro en la Vista Pago

--- Views/Carrito/procesarpago.php
+++ Views/Carrito/procesarpago.php
@@ -28,8 +28,12 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-10 col-xl-7 m-lr-auto m-b-50">
-                <div class="m-l-25 m-r--38 m-lr-0-xl">
+                <div class="bor10 p-lr-40 p-t-30 p-b-40 m-l-63 m-l-25 m-r--38 m-lr-0-xl">
                     <div>
+                        <?php
+                            if(isset($_SESSION['login'])){
+                        ?>
+                
                         <div>
                             <label for="tipopago">Dirección de envío</label>
                             <div class="bor8 bg0 m-b-12">
@@ -38,6 +42,69 @@
                             <div class="bor8 bg0 m-b-22">
                                 <input id="txtCiudad" class="stext-111 cl8 plh3 size-111 p-lr-15" type="text" name="postcode" placeholder="Ciudad / Estado">
                             </div>
+                            <?php } else { ?>
+                                    
+                            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                                <li class="nav-item">
+                                    <a class="nav-link active" id="login-tab" data-toggle="tab" href="#login" role="tab" aria-controls="login" aria-selected="true">Iniciar Sesión</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#registro" role="tab" aria-controls="profile" aria-selected="false">Crear cuenta</a>
+                                </li>
+                            </ul>
+                            <div class="tab-content" id="myTabContent">
+                                <div class="tab-pane fade show active" id="login" role="tabpanel" aria-labelledby="login-tab">
+                                    <br>
+                                    <form id="formLogin">
+                                        <div class="form-group">
+                                            <label for="txtEmail">Usuario</label>
+                                            <input type="email" class="form-control" id="txtEmail" name="txtEmail">
+                                            
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label for="txtPassword">Contraseña</label>
+                                            <input type="password" class="form-control" id="txtPassword" name="txtPassword">
+                                        </div>
+                                        
+                                        <button type="submit" class="btn btn-primary">Iniciar sesión</button>
+
+                                    </form>
+
+                                </div>
+
+                                <div class="tab-pane fade" id="registro" role="tabpanel" aria-labelledby="profile-tab">
+
+                                    <br>
+                                    <form id="formRegister">
+                                        <div class="row">
+                                            <div class="col col-md-6 form-group">
+                                                <label for="txtNombre">Nombres</label>
+                                                <input type="text" class="form-control valid validText" id="txtNombre" name="txtNombre">
+                                            </div>
+                                            <div class="col col-md-6 form-group">
+                                                <label for="txtApellido">Apellidos</label>
+                                                <input type="text" class="form-control valid validText" id="txtApellido" name="txtApellido">
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col col-md-6 form-group">
+                                                <label for="txtTelefono">Teléfono</label>
+                                                <input type="text" class="form-control valid validNumber" id="txtTelefono" name="txtTelefono" required="" onkeypress="return controlTag(event);">
+                                            </div>
+                                            <div class="col col-md-6 form-group">
+                                                <label for="txtEmailCliente">Email</label>
+                                                <input type="email" class="form-control valid validEmail" id="txtEmailCliente" name="txtEmailCliente" required="">
+                                            </div>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary">Regístrate</button>
+
+                                    </form>
+
+                                </div>
+                            </div>
+
+                            <?php } ?>
                         </div>
                     </div>
                 </div>
@@ -89,9 +156,13 @@
                         </div>
                     </div>
 
+                    <?php
+                        if(isset($_SESSION['login'])){
+                    ?>
                     <button type="submit" id="btnComprar" class="flex-c-m stext-101 cl0 size-116 bg3 bor14 hov-btn3 p-lr-15 trans-04 pointer">
                         Pagar
                     </button>
+                    <?php } ?>
                 </div>
             </div>
 
